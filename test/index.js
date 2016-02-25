94b3405b79e7862dd76d453fcb218dec8bd41898
Raúl Sampedro <rsrdesarrollo@gmail.com>
2016-02-25 22:21:50 +0100

    add sign test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 /**
  * Created by rsrdesarrollo on 2/23/16.
  */
+var pem = require('pem');
+var Cfssl_cli = require('../');
 
-var cfssl_cli = require('../');
-
-var cli = new cfssl_cli();
+var cli = new Cfssl_cli();
 
 exports.test_info_with_no_errors = function(test) {
     test.expect(1);
@@ -18,13 +18,53 @@ exports.test_info_with_no_errors = function(test) {
 exports.test_newcert_with_no_errors = function (test){
     test.expect(1);
     var csr = {
+        CN: 'www.example.com',
+        hosts:['www.example.com'],
+        names:[{
+            C: 'CO',
+            ST: 'State',
+            L: 'Location',
+            O: 'Organization',
+            OU: 'Org Unit'
+        }],
 
+        key: {
+            algo: 'rsa',
+            size: 2048
+        }
     };
     cli.newcert(csr, function(err, res){
         test.notEqual(res, null, "Result from newcert query is null.");
         if(!err){
-            console.log(JSON.stringify(res, null, ' '));
+            console.log(res.certificate);
+            //console.log(JSON.stringify(res, null, ' '));
         }
         test.done();
+    });
+};
+
+exports.test_sign_with_no_errors = function (test){
+    test.expect(2);
+
+    pem.createCRS({
+        keyBitsize: 2048,
+        country: 'CO',
+        state: 'State',
+        locality: 'Locality',
+        organization:'Org',
+        organizationUnit: 'OrgUnit',
+        commonName: 'client.cfssl'
+
+    }, function(error, resp){
+
+        cli.sign(resp.csr, function(err, res){
+            test.notEqual(res, null, "Result from sign query is null.");
+            if(!err){
+                pem.readCertificateInfo(res.certificate, function(err, cert){
+                    test.equal(cert.commonName, 'client.cfssl', 'Diferent commonName');
+                    test.done();
+                });
+            }
+        });
     });
 };
